0cc1224a2c6c07aeaad15f723e25e7c75c6bc183
Luiz Felipe 12 <luizfelipe200125@gmail.com>
2025-08-06 17:13:32 -0300

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,45 @@
+package atvquartaHudson;
+import java.util.Scanner;
+public class Menu {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); 
+		UsuarioService usuarioService = new UsuarioService();		
+		
+		int opcao;
+		
+		do {
+			
+			        System.out.println("======= MENO ========");
+					System.out.println("1. Cadastrar usuário");
+					System.out.println("2. Listar usuários");
+					System.out.println("3. Sair");
+					System.out.print("Escolha uma opção: ");
+					
+					opcao = scanner.nextInt(); 
+					scanner.nextLine();
+		
+		switch (opcao) { 
+		            
+		case 1:
+			System.out.print("Digite o nome do usuário: ");
+			String nome = scanner.nextLine();
+			usuarioService.cadastrarUsuario(nome);
+			break;
+			case 2:
+			usuarioService.listarUsuarios();
+			break;
+			case 3:
+			System.out.println("Encerrando o programa...");
+			break;
+			default:
+		
+		} 
+		System.out.println();
+		
+		}while(opcao != 3); 
+		
+		scanner.close();
+  }
+
+}
